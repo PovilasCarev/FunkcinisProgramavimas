e950f1a64ff87b0e1b90277603ffda4344c145de
PovilasCa <povilas.ca@gmail.com>
2024-10-21 11:06:16 +0000

    Updated Lib2.hs and Spec,hs files

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -1,44 +1,120 @@
-{-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+
 module Lib2
-    ( Query(..),
+  ( Query (..),
+    Vinyl (..), -- Add this line to export Vinyl
     parseQuery,
-    State(..),
+    State (..),
     emptyState,
-    stateTransition
-    ) where
+    stateTransition,
+  )
+where
 
--- | An entity which represets user input.
--- It should match the grammar from Laboratory work #1.
--- Currently it has no constructors but you can introduce
--- as many as needed.
+import Data.List (isPrefixOf)
+import Data.Maybe (fromMaybe)
+
+-- | Represents different types of user queries according to the grammar.
 data Query
+  = Add Vinyl
+  | Remove Vinyl
+  | Update Vinyl
+  | ViewAll
+  | ViewByGenre String
+  | ViewByArtist String
+  | ViewByYear Int
+  | SubCommand Query Query
+  deriving (Eq, Show)
 
--- | The instances are needed basically for tests
-instance Eq Query where
-  (==) _ _= False
+-- | Vinyl record data.
+data Vinyl = Vinyl
+  { vinylArtist :: String, -- Renamed from 'artist' to 'vinylArtist'
+    title :: String,
+    vinylYear :: Int, -- Renamed from 'year' to 'vinylYear'
+    vinylGenre :: String -- Renamed from 'genre' to 'vinylGenre'
+  }
+  deriving (Eq, Show)
 
-instance Show Query where
-  show _ = ""
-
--- | Parses user's input.
--- The function must have tests.
+-- | Parses user's input into a Query.
 parseQuery :: String -> Either String Query
-parseQuery _ = Left "Not implemented 2"
+parseQuery input
+  | "Add " `isPrefixOf` input = parseAdd (drop 4 input)
+  | "Remove " `isPrefixOf` input = parseRemove (drop 7 input)
+  | "Update " `isPrefixOf` input = parseUpdate (drop 7 input)
+  | "View " `isPrefixOf` input = parseView (drop 5 input)
+  | otherwise = Left "Unknown command"
 
--- | An entity which represents your program's state.
--- Currently it has no constructors but you can introduce
--- as many as needed.
-data State
+-- | Parsing an 'Add' command with a vinyl record.
+parseAdd :: String -> Either String Query
+parseAdd input = do
+  vinyl <- parseVinyl input
+  return $ Add vinyl
 
--- | Creates an initial program's state.
--- It is called once when the program starts.
+-- | Parsing a 'Remove' command with a vinyl record.
+parseRemove :: String -> Either String Query
+parseRemove input = do
+  vinyl <- parseVinyl input
+  return $ Remove vinyl
+
+-- | Parsing an 'Update' command with a vinyl record.
+parseUpdate :: String -> Either String Query
+parseUpdate input = do
+  vinyl <- parseVinyl input
+  return $ Update vinyl
+
+-- | Parsing a 'View' command with criteria.
+parseView :: String -> Either String Query
+parseView input
+  | input == "all records" = Right ViewAll
+  | "in " `isPrefixOf` input = Right $ ViewByGenre (drop 3 input)
+  | "by " `isPrefixOf` input = Right $ ViewByArtist (drop 3 input)
+  | "released in " `isPrefixOf` input = do
+      let yearStr = drop 12 input
+      year <- case reads yearStr of
+        [(y, "")] -> Right y
+        _ -> Left "Invalid year"
+      Right $ ViewByYear year
+  | otherwise = Left "Invalid view command"
+
+-- | Parsing a vinyl record from the input.
+parseVinyl :: String -> Either String Vinyl
+parseVinyl input = do
+  let (artist, rest1) = span (/= '_') input
+  let (vinylTitle, rest2) = span (/= '_') (drop 1 rest1)
+  let (yearStr, rest3) = span (/= '_') (drop 1 rest2)
+  let parsedGenre = drop 1 rest3
+  parsedYear <- case reads yearStr of
+    [(y, "")] -> Right y
+    _ -> Left "Invalid year"
+  return $ Vinyl artist vinylTitle parsedYear parsedGenre
+
+-- | Represents the state of the system, holding a collection of vinyl records.
+newtype State = State {vinylCollection :: [Vinyl]} deriving (Show)
+
+-- | Creates an initial program state.
 emptyState :: State
-emptyState = error "Not implemented 1"
+emptyState = State []
 
--- | Updates a state according to a query.
--- This allows your program to share the state
--- between repl iterations.
--- Right contains an optional message to print and
--- an updated program's state.
+-- | Updates the program state according to the query.
 stateTransition :: State -> Query -> Either String (Maybe String, State)
-stateTransition _ _ = Left "Not implemented 3"
+stateTransition state (Add vinyl) =
+  let newState = state {vinylCollection = vinyl : vinylCollection state}
+   in Right (Just "Vinyl added", newState)
+stateTransition state (Remove vinyl) =
+  let newCollection = filter (/= vinyl) (vinylCollection state)
+   in Right (Just "Vinyl removed", state {vinylCollection = newCollection})
+stateTransition state ViewAll =
+  Right (Just $ show (vinylCollection state), state)
+stateTransition state (ViewByGenre genre) =
+  let filtered = filter (\v -> genre == vinylGenre v) (vinylCollection state)
+   in Right (Just $ show filtered, state)
+stateTransition state (ViewByArtist artist) =
+  let filtered = filter (\v -> artist == vinylArtist v) (vinylCollection state)
+   in Right (Just $ show filtered, state)
+stateTransition state (ViewByYear year) =
+  let filtered = filter (\v -> year == vinylYear v) (vinylCollection state)
+   in Right (Just $ show filtered, state)
+stateTransition state (SubCommand q1 q2) = do
+  (msg1, newState1) <- stateTransition state q1
+  (msg2, newState2) <- stateTransition newState1 q2
+  return (Just $ fromMaybe "" msg1 ++ "; " ++ fromMaybe "" msg2, newState2)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-import Test.Tasty ( TestTree, defaultMain, testGroup )
-import Test.Tasty.HUnit ( testCase, (@?=) )
 
 import Lib1 qualified
-import Lib2 qualified
+import Lib2
+import Lib2 (Query (..), State (..), Vinyl (..), emptyState, parseQuery, stateTransition)
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
 
 main :: IO ()
 main = defaultMain tests
@@ -12,11 +13,39 @@ tests :: TestTree
 tests = testGroup "Tests" [unitTests]
 
 unitTests :: TestTree
-unitTests = testGroup "Lib1 tests"
-  [ testCase "List of completions is not empty" $
-      null Lib1.completions @?= False,
-    testCase "Parsing case 1 - give a better name" $
-      Lib2.parseQuery "" @?= (Left "Some error message"),
-    testCase "Parsing case 2 - give a better name" $
-      Lib2.parseQuery "o" @?= (Left "Some error message")
-  ]
+unitTests =
+  testGroup
+    "Lib2 tests"
+    [ -- Existing test cases
+      testCase "Parsing case 1 - Empty input" $
+        Lib2.parseQuery "" @?= (Left "Unknown command"),
+      testCase "Parsing case 2 - Single character input" $
+        Lib2.parseQuery "o" @?= (Left "Unknown command"),
+      -- New test cases for parseQuery
+      testCase "Parsing case 3 - Valid Add command" $
+        parseQuery "Add Artist_Title_2023_Genre" @?= Right (Add (Vinyl "Artist" "Title" 2023 "Genre")),
+      testCase "Parsing case 4 - Invalid Add command (missing info)" $
+        parseQuery "Add Artist_Title__Genre" @?= Left "Invalid year",
+      testCase "Parsing case 5 - Valid View all command" $
+        parseQuery "View all records" @?= Right ViewAll,
+      testCase "Parsing case 6 - Valid Remove command" $
+        parseQuery "Remove Artist_Title_2023_Genre" @?= Right (Remove (Vinyl "Artist" "Title" 2023 "Genre")),
+      -- New test cases for stateTransition
+      testCase "StateTransition - Add Vinyl to an empty state" $ do
+        let vinyl = Vinyl "Artist" "Title" 2023 "Genre"
+            result = stateTransition emptyState (Add vinyl)
+        case result of
+          Right (_, newState) -> vinylCollection newState @?= [vinyl],
+      testCase "StateTransition - Remove Vinyl from state" $ do
+        let vinyl = Vinyl "Artist" "Title" 2023 "Genre"
+            state = State [vinyl]
+            result = stateTransition state (Remove vinyl)
+        case result of
+          Right (_, newState) -> vinylCollection newState @?= [],
+      testCase "StateTransition - View all records in state" $ do
+        let vinyl = Vinyl "Artist" "Title" 2023 "Genre"
+            state = State [vinyl]
+            result = stateTransition state ViewAll
+        case result of
+          Right (Just output, _) -> output @?= show [vinyl]
+    ]
